70b4d7834b277ba4e7865480d5f9f9f47671ebec
Gus <gustavo_roger@yahoo.com>
2022-10-11 21:14:51 +0800

    events controller complete and tested on postman

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -31,15 +31,51 @@ events.get('/:id', async (req, res) => {
 
 // Create Event
 
-
+events.post('/', async (req, res) => {
+    try {
+        const newEvent = await Event.create(req.body)
+        res.status(200).json({
+            message: 'Successfully created new event',
+            data: newEvent
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 
 // Update Event
 
-
+events.put('/:id', async(req, res) => {
+    try {
+        const updateEvent = await Event.update(req.body, {
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updateEvent} Event(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 
 // Delete Event
 
-
+events.delete('/:id', async(req, res) => {
+    try {
+        const deletedEvents = await Event.destroy({
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedEvents} Event(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
 
 // Export
 module.exports = events
